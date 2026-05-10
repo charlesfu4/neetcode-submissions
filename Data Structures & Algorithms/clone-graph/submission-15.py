24106a2c05b2095de62026dfb49d2b4f57d8eaee
Chu-Cheng Fu <ccfu@kth.se>
2026-05-10 22:31:11 +0200

    Add: clone-graph - submission-15

---
+++ Data Structures & Algorithms/clone-graph/submission-15.py
@@ -0,0 +1,22 @@
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+
+        if not node:
+            return None
+
+        first_node = node
+        q = deque([node])
+        visited = {}
+        visited[node] = Node(node.val)
+
+
+
+        while q:
+            curr = q.popleft()
+            for neighbor in curr.neighbors:
+                if neighbor not in visited:
+                    visited[neighbor] = Node(neighbor.val)
+                    q.append(neighbor)
+                visited[curr].neighbors.append(visited[neighbor])
+        return visited[first_node]
